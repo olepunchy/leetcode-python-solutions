a06283cabcdbd9c94ee9323d78051f5012bd84cb
olepunchy <olepunchy@gmail.com>
2022-01-27 17:43:15 -0600

    Updated

--- first_bad_version/main.py
+++ first_bad_version/main.py
@@ -1,8 +1,5 @@
 """
-<<<<<<< HEAD
-TODO: Complete this solution
-"""
-=======
+Description:
 You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
 
 Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
@@ -46,4 +43,3 @@ class Solution(object):
                 low = mid + 1
 
         return low
->>>>>>> 650a776 (Updated)
