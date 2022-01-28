765bd1bdc8228d637dd76ab8ef2e72dccafa1ecd
olepunchy <olepunchy@gmail.com>
2022-01-27 19:47:33 -0600

    Updated

---
+++ move_zeroes/main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+from black import List
+
+"""
+Given an integer array nums, move all 0's to the end of it while maintaining the
+relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+
+
+Example 1:
+---------
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+---------
+
+Input: nums = [0]
+Output: [0]
+"""
+
+
+class Solution:
+    def move_zeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        # Create a new list filled with zeroes of the same length as nums
+        length = len(nums)
+        new_nums = [0] * length
+
+        insert_index = 0
+
+        for i in range(length):
+            if nums[i] != 0:
+                # Insert the number towards the beginning of the list
+                new_nums.insert(insert_index, nums[i])
+                # The list is now one larger, pop an element off the back
+                new_nums.pop()
+                # Increment the index for insertion
+                insert_index += 1
+
+        nums[:] = new_nums
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    numbers = [0, 1, 0, 3, 12]
+
+    print(f"Before: {numbers}")
+
+    solution.move_zeroes(numbers)
+
+    print(f"After: {numbers}")
